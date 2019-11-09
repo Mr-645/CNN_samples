b36e8805b5222de11d186ab57a60637702e48317
Mr-645 <18206187+Mr-645@users.noreply.github.com>
2019-11-09 20:32:54 +1300

    Update and rename TF_cat_dog.py to TF_cat_dog_train.py

--- TF_cat_dog_train.py
+++ TF_cat_dog_train.py
@@ -1,32 +1,30 @@
-from keras.preprocessing.image import ImageDataGenerator
+#Tensorflow, train a binary classifier
+
+rom keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 from keras import backend
 import numpy as np
 from keras.preprocessing import image
 from keras.callbacks import TensorBoard
-from keras.datasets import imdb  # data loading --- train_data=imdb.load_data('yourdatapath')
 import time
+import pickle
 
 # tensorboard CLI entry: tensorboard --logdir=logs/ --host 192.168.1.73 --port 6006
 
 # Setup
-img_width, img_height = 150, 150
+img_width, img_height = 150, 150  # the dimensions need to be declared first
 
-training_data_folder = "C:/Users/drift/Downloads/data/train"
-validation_data_folder = "C:/Users/drift/Downloads/data/validation"
+training_data_folder = ""  # folder containing folders of photos of cats and dogs
+validation_data_folder = ""  # folder containing folders of DIFFERENT photos of cats and dogs
 
-# training_data_folder = "C:/Users/drift/Downloads/ImageAssistant Batch Image Downloader"
-# validation_data_folder = "C:/Users/drift/Downloads/ImageAssistant Batch Image Downloader"
-
-# NAME = "Cats-vs-dogs-CNN-{}".format(int(time.time()))
-NAME = "5-Fruit-CNN-{}".format(int(time.time()))
+NAME = "5-Fruit-CNN-{}".format(int(time.time()))  # Tensorboard log file name
 tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
 
-number_of_training_samples = 130  # 1000
-number_of_validation_samples = 130  # 100
-number_of_epochs = 30  # 50, try 31
-the_batch_size = 130  # 20
+number_of_training_samples = 130
+number_of_validation_samples = 130
+number_of_epochs = 30
+the_batch_size = 130
 
 
 def do_training():
@@ -34,7 +32,7 @@ def do_training():
     if backend.image_data_format() == 'channels_first':
         input_shape = (3, img_width, img_height)
     else:
-        input_shape = (img_width, img_height, 3)  # 50/130,3
+        input_shape = (img_width, img_height, 3)
 
     train_datagen = ImageDataGenerator(
         rescale=1. / 255,
@@ -42,23 +40,22 @@ def do_training():
         zoom_range=0.2,
         horizontal_flip=True)
 
-    test_datagen = ImageDataGenerator(rescale=1. / 255)  # for testing only
-
     training_data_generator = train_datagen.flow_from_directory(
         training_data_folder,
         target_size=(img_width, img_height),
         batch_size=the_batch_size,
-        class_mode="binary")  # was binary before
+        class_mode="binary")
 
     validation_data_generator = train_datagen.flow_from_directory(
         validation_data_folder,
         target_size=(img_width, img_height),
         batch_size=the_batch_size,
-        class_mode="binary")  # was binary before
+        class_mode="binary")
 
     # make the model itself
     the_model = Sequential()
-    the_model.add(Convolution2D(32, (3, 3), activation="relu", input_shape=input_shape))  # number of features and 3x3 pixel matrix
+    the_model.add(Convolution2D(32, (3, 3), activation="relu",
+                                input_shape=input_shape))  # number of features and 3x3 pixel matrix
     the_model.add(MaxPooling2D(pool_size=(2, 2)))
 
     the_model.summary()
@@ -76,7 +73,6 @@ def do_training():
 
     the_model.summary()
     the_model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["accuracy"])
-    # the_model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
     # the training itself
     the_model.fit_generator(  # connects nodes
@@ -92,23 +88,17 @@ def do_training():
 
 
 def predict_from_an_image(the_model, location):
-    # print("\r" + str(location))
-    # Feed it an image to do a prediction on
     image_to_predict = image.load_img(location, target_size=(150, 150))
     image_to_predict = image.img_to_array(image_to_predict)
     image_to_predict = np.expand_dims(image_to_predict, axis=0)
     # Do data prediction
     the_result = the_model.predict(image_to_predict)
     return the_result
-    
-the_result = m.predict_from_an_image(
-    the_model=my_model_1,
-    location="C:/Users/drift/Desktop/dog.jpg")
 
-if the_result[0][0] == 1:
-    the_prediction = "dog"
-else:
-    the_prediction = "cat"
+# Perform training
+the_model = do_training()
 
-print(the_result)
-print(the_prediction)
+# Save model
+pickle_out = open("the_model_i_saved.pickle", "wb")
+pickle.dump(the_model, pickle_out)
+pickle_out.close()
